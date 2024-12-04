423ceee7abb5a1f2cf9437cf7563e2a8ed258d30
phoemphanith <phanith1826@gmail.com>
2024-12-04 10:12:25 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dynamic-forms-sample-app';
+  dynamicForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.dynamicForm = this.fb.group({
+      name: [''],
+      email: [''],
+      fields: this.fb.array([]),
+    });
+  }
+
+  get fields(): FormArray {
+    return this.dynamicForm.get("fields") as FormArray;
+  }
+
+  addField() {
+    const fieldGroup = this.fb.group({
+      label: [""],
+      value: [""]
+    });
+    this.fields.push(fieldGroup);
+  }
+
+  removeField(index: number) {
+    this.fields.removeAt(index);
+  }
+
+  submitForm() {
+    console.log(this.dynamicForm.value);
+  }
 }
